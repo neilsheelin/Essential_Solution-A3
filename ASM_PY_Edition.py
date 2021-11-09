21551ac1f67d0de1ead0d71f0c6a60137be6216b
JyeThomson <jyelthomson@gmail.com>
2021-11-09 23:33:09 +1100

    Update ASM_PY_Edition.py
    Working on creating a database file to load from and save to.

--- ASM_PY_Edition.py
+++ ASM_PY_Edition.py
@@ -10,35 +10,44 @@ class UI:
 
         #menu decor
         bars = "\n================================================================"
-
         #initial login menu assets
         login_menu = bars
         login_menu += "\n\nWelcome to the Australian Sports Manager (ASM)"
         login_menu += "\n" + bars
         login_menu += "\n\nExisting user? (Y/N): "
-
         #create user menu assets
         create_um = bars
         create_um += "\n\nCreate a New ASM User"
         create_um += bars
-    
         #standard menu assets
         std_menu = ""
-
         #admin menu assets
         adm_menu = ""
 
+
+
+
         option = UI.get_yn(login_menu)
+        UM = UserManager()
+        
 
-        if (option == "y"):
-            user_name = UI.get_string("\nPlease enter username: ")
+        if (option == "n"):
+            pass
+
+        elif (option == "y"):
+            username = UI.get_string("\nPlease enter username: ")
             password = UI.get_string("Please enter password: ")
-
-        elif (option == "n"):
+            UM.log_in(username, password)
             pass
 
         else:
             pass
+
+
+
+
+
+
 
     #returns an integer from the user
     def get_integer(prompt):
@@ -61,21 +70,58 @@ class UI:
             sys.stdout.write("Please enter Y or N: ")
             user_input = sys.stdin.readline().lower().strip()
         return user_input
+
+    def clear_console():
+        os.system("cls" if os.name == "nt" else "clear")
             
 
 
 class User:
 
-    def __init__(self,admin,username,email,mobile,available):
+    def __init__(self,admin,username,password,email,mobile,available):
         self.admin = False
         self.username = username
+        self.password = password
         self.email = email
         self.mobile = mobile
         self.available = ""
 
 class UserManager:
 
-    def __init__(self)
+    def __init__(self):
+        self.users = []
+
+    def log_in(self, username, password):
+        print(username + " " + password)
+
+    def add_user(self,admin,username,password,email,mobile,available):
+        self.users.append(User(admin,username,password,email,mobile,available))
+
+    def save_file(self):
+        pass
+
+    def load_file(self):
+        try:
+            with open("userdata.csv", r) as load_csv:
+                self.users = []
+                i = 0
+                while (i == 0):
+                    try:
+                        load_contents = load_csv.readline()
+                        attributes = load_csv.split(",")
+                        admin = attributes[0]
+                        username = attributes[1]
+                        password = attributes[2]
+                        email = attributes[3]
+                        mobile = attributes[4]
+                        available = attributes[5]
+                        self.add_user(admin,username,password,email,mobile,available)
+                    except:
+                        i = 1
+        except:
+            sys.stdout.write("userdata.csv could not be found.")
+                    
+                
 
 initialise = UI()
         
